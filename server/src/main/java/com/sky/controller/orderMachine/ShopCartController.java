e21f0eabeeb9a6688b20665447a770f1a5f4a114
koni5 <2824245480@qq.com>
2024-05-22 17:34:01 +0800

    "点餐机添加菜品到购物车"完成

---
+++ server/src/main/java/com/sky/controller/orderMachine/ShopCartController.java
@@ -0,0 +1,29 @@
+package com.sky.controller.orderMachine;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShopCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController("machineShopCartController")
+@RequestMapping("/machine/shoppingCart")
+@Slf4j
+public class ShopCartController {
+    @Autowired
+    private ShopCartService shopCartService;
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加的菜品id为:{},口味为:{},所属店铺id为:{}", shoppingCartDTO.getDishId(), shoppingCartDTO.getDishFlavor(), shoppingCartDTO.getShopId());
+        shopCartService.add(shoppingCartDTO);
+        return Result.success();
+    }
+}
--- server/src/main/java/com/sky/controller/user/ShopCartController.java
+++ server/src/main/java/com/sky/controller/user/ShopCartController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("ShopCartController")
+@RestController("userShopCartController")
 @RequestMapping("/user/shopCart")
 @Slf4j
 public class ShopCartController {
